ac5254d862673161903e27af94d934081ce86965
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2025-11-01 20:30:15 +0330

    Fix Gradle plugin version compatibility and repository configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
