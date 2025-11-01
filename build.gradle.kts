f33b6795fa0bc6f722a526fb81dfab0bc6aa316f
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2025-11-01 21:49:45 +0330

    Fix root build.gradle.kts - remove repositories and use proper buildscript dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+        classpath("com.android.tools.build:gradle:8.3.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
